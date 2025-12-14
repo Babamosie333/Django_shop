8d352f41a5bc566006b55355b76f9941e4feb64e
Babamosie333 <vikramsingh14052006@gmail.com>
2025-12-14 21:13:08 +0530

    change

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,9 @@ DEBUG = os.environ.get('DJANGO_DEBUG', "True") == "True"
 SECRET_KEY = 'django-insecure-!k#g@-)v9nd@x*vip_j6!a(f116#g3hhl1zi!k8!=^r&w+l-*@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Babamosie333.pythonanywhere.com']
 
 
 # Application definition
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [BASE_DIR / 'static',]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
